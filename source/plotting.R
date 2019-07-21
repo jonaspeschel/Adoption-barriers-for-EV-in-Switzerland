b7a5b64fcc0c86f4e790cf14e746b0632b4d3b5e
jonaspeschel <pescheljonas@gmail.com>
2019-07-21 10:31:35 +0200

    - fixed all plots: rearranging item order

--- source/plotting.R
+++ source/plotting.R
@@ -6,48 +6,62 @@
 ######################
 
 # convert data for plotting
-d_gender_melt <- melt(data = d_gender, id.vars = "Gender Groups",  measure.vars = c("Percentage of Survey", "Percentage of 2015 Mobility Census"))
-d_gender_melt$value <- d_gender_melt$value*100
+d_gender_plot <- melt(data = d_gender, id.vars = "Gender Groups",  measure.vars = c("Percentage of Survey", "Percentage of 2015 Mobility Census"))
+d_gender_plot$value <- round(d_gender_plot$value*100, digits=1)
 
 # plot data
-plot_age <- ggplot(
-  data = d_gender_melt, 
+plot_gender <- ggplot(
+  data = d_gender_plot, 
   aes(x = `Gender Groups`, y = value, fill = variable)
 ) + 
   geom_bar(colour="black", stat="identity", position = position_dodge()) + # set black outline of bars, height of bars, position dodge
   xlab("Gender groups" ) + # set x axis label
+  geom_text(aes(label= value), position = position_dodge(width=0.9), vjust = -0.25) + # Add value on top of bars
   ylab("Percentage of total") + # set y axis label
   ggtitle("Gender distribution") + # set title
-  scale_fill_brewer(palette = "Blues") # set color pattern
+  scale_fill_brewer(palette = "Blues") + # set color pattern
+  theme(text = element_text(size=18), # change font size to 20
+        panel.grid.major = element_blank(), # remove background grid
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        axis.line = element_line(colour = "black")) # add axis line
 
 # display plot
-print(plot_age)
+print(plot_gender)
 dev.off() # removes displayed graphic
 
 # save plot in file
 ggsave("images/plot_gender.png", width = 10, height = 5)
 
 # removal of temporary files
-rm(d_gender_melt)
+rm(d_gender_plot,
+   plot_gender)
 
 ######################
 # Q5 age
 ######################
 
 # convert data for plotting
-d_age_melt <- melt(data = d_age, id.vars = "Age Groups",  measure.vars = c("Percentage of Survey", "Percentage of 2015 Mobility Census"))
-d_age_melt$value <- d_age_melt$value*100
+d_age_plot <- melt(data = d_age, id.vars = "Age Groups",  measure.vars = c("Percentage of Survey", "Percentage of 2015 Mobility Census"))
+d_age_plot$value <- round(d_age_plot$value*100, digits=1)
 
 # plot data
 plot_age <- ggplot(
-  data = d_age_melt, 
+  data = d_age_plot, 
   aes(x = `Age Groups`, y = value, fill = variable)
 ) + 
   geom_bar(colour="black", stat="identity", position = position_dodge()) + # set black outline of bars, height of bars, position dodge
   xlab("Age groups") + # set x axis label
+  geom_text(aes(label= value), position = position_dodge(width=0.9), vjust = -0.25) + # Add value on top of bars
   ylab("Percentage of total") + # set y axis label
   ggtitle("Age distribution") + # set title
-  scale_fill_brewer(palette = "Blues") # set color pattern
+  scale_fill_brewer(palette = "Blues") + # set color pattern
+  theme(text = element_text(size=18), # change font size to 20
+      panel.grid.major = element_blank(), # remove background grid
+      panel.grid.minor = element_blank(),
+      panel.background = element_blank(),
+      axis.line = element_line(colour = "black")) # add axis line
+
 
 # display plot
 print(plot_age)
@@ -57,24 +71,8 @@ dev.off() # removes displayed graphic
 ggsave("images/plot_age.png", width = 10, height = 5)
 
 # removal of temporary files
-rm(d_age_melt)
-
-############ old stuf
-op <- par(mfrow = c(3, 3))
-
-
-hist(d$'Q18_PT-spend')
-hist(d_age$`Percentage of Survey`)
-
-hist(d$'Q2_lang-DE')
-hist(d[[10]])
-hist(d[[11]])
-for (i in 10:15){
-  hist(d[[i]])
-  boxplot(d[[i]])  
-}
-boxplot(d$"Q5_age")
-############3 end of old stuff
+rm(d_age_plot,
+   plot_age)
 
 ######################
 # Q6 regions
@@ -91,12 +89,12 @@ d_region_temp$`Region Groups` <- c(
   "Central Switzerland",
   "Ticino"
 )
-d_region_melt <- melt(data = d_region_temp, id.vars = "Region Groups",  measure.vars = c("Percentage of Survey", "Percentage of 2015 Mobility Census"))
-d_region_melt$value <- d_region_melt$value*100
+d_region_plot <- melt(data = d_region_temp, id.vars = "Region Groups",  measure.vars = c("Percentage of Survey", "Percentage of 2015 Mobility Census"))
+d_region_plot$value <- round(d_region_plot$value*100, digits = 1)
 
 # plot data
 plot_region <- ggplot(
-  data = d_region_melt, 
+  data = d_region_plot, 
   aes(x = `Region Groups`, y = value, fill = variable)
 ) + 
   geom_bar(colour="black", stat="identity", position = position_dodge()) + # set black outline of bars, height of bars, position dodge
@@ -108,22 +106,29 @@ plot_region <- ggplot(
                             "Easter Switzerland",
                             "Central Switzerland",
                             "Ticino")) + # rearrange values on x-axis
+  geom_text(aes(label= value), position = position_dodge(width=0.9), vjust = -0.25) + # Add value on top of bars
   ylab("Percentage of total") + # set y axis label
   ggtitle("Regional distribution") + # set title
   scale_fill_brewer(palette = "Blues") + # set color pattern
-  theme(axis.text.x = element_text(angle = 90)) # rotates x-axis text by 90 degrees
+  theme(text = element_text(size=18), # change font size to 20
+        axis.text.x = element_text(angle = 90), # rotates x-axis text by 90 degrees
+        panel.grid.major = element_blank(), # remove background grid
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        axis.line = element_line(colour = "black")) # add axis line
 
 # display plot
 print(plot_region)
 dev.off() # removes displayed graphic
 
 # save plot in file
-ggsave("images/plot_region.png", width = 10, height = 5)
+ggsave("images/plot_region.png", width = 10, height = 8)
 
 # removal of temporary files
 rm(
   d_region_temp,
-  d_region_melt
+  d_region_plot,
+  plot_region
    )
 
 ######################
@@ -131,12 +136,12 @@ rm(
 ######################
 
 # convert data for plotting
-d_income_melt <- melt(data = d_income, id.vars = "Income Groups",  measure.vars = c("Percentage of Survey", "Percentage of 2015 Mobility Census"))
-d_income_melt$value <- d_income_melt$value*100
+d_income_plot <- melt(data = d_income, id.vars = "Income Groups",  measure.vars = c("Percentage of Survey", "Percentage of 2015 Mobility Census"))
+d_income_plot$value <- round(d_income_plot$value*100, digits = 1)
 
 # plot data
 plot_income <- ggplot(
-  data = d_income_melt, 
+  data = d_income_plot, 
   aes(x = `Income Groups`, y = value, fill = variable, factor(`Income Groups`))
 ) + 
   geom_bar(colour="black", stat="identity", position = position_dodge()) + # set black outline of bars, height of bars, position dodge
@@ -146,10 +151,16 @@ plot_income <- ggplot(
                             "8'000 - 12'000",
                             "Above 12'000",
                             "Don't know/no comment")) + # rearrange values on x-axis
+  geom_text(aes(label= value), position = position_dodge(width=0.9), vjust = -0.25) + # Add value on top of bars
   ylab("Percentage of total") + # set y axis label
   ggtitle("Income distribution") + # set title
   scale_fill_brewer(palette = "Blues") + # set color pattern
-  theme(axis.text.x = element_text(angle = 90)) # rotates x-axis text by 90 degrees
+  theme(text = element_text(size=18), # change font size to 20
+        axis.text.x = element_text(angle = 90), # rotates x-axis text by 90 degrees
+        panel.grid.major = element_blank(), # remove background grid
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        axis.line = element_line(colour = "black")) # add axis line
 
 
 # display plot
@@ -157,10 +168,11 @@ print(plot_income)
 dev.off() # removes displayed graphic
 
 # save plot in file
-ggsave("images/plot_income.png", width = 10, height = 5)
+ggsave("images/plot_income.png", width = 10, height = 8)
 
 # removal of temporary files
-rm(d_income_melt)
+rm(d_income_plot,
+   plot_income)
 
 ######################
 # Q12 EV driving experience
@@ -378,7 +390,7 @@ rm(d_mob_subscr_pi_plot,
 
 # Convert data for plotting
 d_EV_adv_plot <- d_EV_adv
-d_EV_adv_plot$`Percentage of Survey` <- d_EV_adv_plot$`Percentage of Survey`*100
+d_EV_adv_plot$`Percentage of Survey` <- round(d_EV_adv_plot$`Percentage of Survey`*100, digits = 1)
 
 # plot data
 plot_EV_adv <- ggplot(
@@ -388,17 +400,23 @@ plot_EV_adv <- ggplot(
   geom_bar(colour="black", fill = "Blue", stat="identity", position = position_dodge()) + # set black outline of bars, height of bars, position dodge
   xlab("EV advantages choices") + # set x axis label
   scale_x_discrete(limits=c("Environmental impact",
+                            "Low CO2 emissions",
                             "Little noise",
                             "Improved air quality",
-                            "Low CO2 emissions",
-                            "Pleasant driving experience",
                             "Savings in operating costs",
                             "Savings in total cost of ownership",
+                            "Pleasant driving experience",
                             "Other")) + # rearrange values on x-axis
+  geom_text(aes(label= `Percentage of Survey`), position = position_dodge(width=0.9), vjust = -0.25) + # Add value on top of bars
   ylab("Percentage of total") + # set y axis label
   ggtitle("EV advantages share of choices") + # set title
   scale_fill_brewer(palette = "Blues") + # set color pattern
-  theme(axis.text.x = element_text(angle = 90)) # rotates x-axis text by 90 degrees
+  theme(text = element_text(size=18), # change font size to 20
+        axis.text.x = element_text(angle = 90), # rotates x-axis text by 90 degrees
+        panel.grid.major = element_blank(), # remove background grid
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        axis.line = element_line(colour = "black")) # add axis line
 
 
 # display plot
@@ -406,10 +424,11 @@ print(plot_EV_adv)
 dev.off() # removes displayed graphic
 
 # save plot in file
-ggsave("images/plot_EV_adv.png", width = 10, height = 5)
+ggsave("images/plot_EV_adv.png", width = 10, height = 10)
 
 # removal of temporary files
-rm(d_EV_adv_plot)
+rm(d_EV_adv_plot,
+   plot_EV_adv)
 
 ######################
 # Q41 EV disadvantages
@@ -417,10 +436,10 @@ rm(d_EV_adv_plot)
 
 # Convert data for plotting
 d_EV_disadv_plot <- d_EV_disadv
-d_EV_disadv_plot$`Percentage of Survey` <- d_EV_disadv_plot$`Percentage of Survey`*100
+d_EV_disadv_plot$`Percentage of Survey` <- round(d_EV_disadv_plot$`Percentage of Survey`*100, digits = 1)
 
 # plot data
-d_EV_disadv_plot <- ggplot(
+plot_EV_disadv <- ggplot(
   data = d_EV_disadv_plot, 
   aes(x = `EV disadvantages groups`, y = `Percentage of Survey`, factor(`EV disadvantages groups`))
 ) + 
@@ -428,25 +447,32 @@ d_EV_disadv_plot <- ggplot(
   xlab("EV disadvantages choices") + # set x axis label
   scale_x_discrete(limits=c("Battery range",
                             "Charging time",
-                            "Charging infrastructure: public charging infrastructure",
-                            "Charging infrastructure: at home",
-                            "Charging infrastructure: at work",
                             "High purchasing price of the vehicle",
+                            "Charging infrastructure: at home",
+                            "Charging infrastructure: public charging infrastructure",
+                            "Charging infrastructure: at work",
                             "Lack of information about electric vehicles",
                             "Limited choice of vehicle models",
                             "Other")) + # rearrange values on x-axis
+  geom_text(aes(label= `Percentage of Survey`), position = position_dodge(width=0.9), vjust = -0.25) + # Add value on top of bars
   ylab("Percentage of total") + # set y axis label
   ggtitle("EV disadvantages share of choices") + # set title
   scale_fill_brewer(palette = "Blues") + # set color pattern
-  theme(axis.text.x = element_text(angle = 90)) # rotates x-axis text by 90 degrees
+  theme(text = element_text(size=18), # change font size to 20
+        axis.text.x = element_text(angle = 90), # rotates x-axis text by 90 degrees
+        panel.grid.major = element_blank(), # remove background grid
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        axis.line = element_line(colour = "black")) # add axis line
 
 
 # display plot
-print(d_EV_disadv_plot)
+print(plot_EV_disadv)
 dev.off() # removes displayed graphic
 
 # save plot in file
-ggsave("images/plot_EV_disadv.png", width = 10, height = 5)
+ggsave("images/plot_EV_disadv.png", width = 10, height = 10)
 
 # removal of temporary files
-rm(d_EV_disadv_plot)
+rm(d_EV_disadv_plot,
+   plot_EV_disadv)
